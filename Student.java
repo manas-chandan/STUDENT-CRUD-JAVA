1d44186469ebeee89091f625138d89c2619c1431
Manas Barik <77716299+manas-chandan@users.noreply.github.com>
2023-09-11 15:27:39 +0530

    Student add

---
+++ Student.java
@@ -0,0 +1,30 @@
+public class Student {
+	private String name;
+	private int roll;
+	private double fee;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getRoll() {
+		return roll;
+	}
+
+	public void setRoll(int roll) {
+		this.roll = roll;
+	}
+
+	public double getFee() {
+		return fee;
+	}
+
+	public void setFee(double fee) {
+		this.fee = fee;
+	}
+
+}
